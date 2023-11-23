c32b0275355a83ae2501b1b22c3b0fec41d5a590
AmyMialee <10999535+AmyMialee@users.noreply.github.com>
2023-11-23 12:42:36 +0000

    Fine tuning

--- Assets/Code/Scripts/Util/MathHelper.cs
+++ Assets/Code/Scripts/Util/MathHelper.cs
@@ -13,15 +13,33 @@ namespace Code.Scripts.Util {
 		public static double lerp(double a, double b, double t) {
 			return a + (b - a) * t;
 		}
-		
+
+		public static int largest(int a, int b) {
+			var absA = Math.Abs(a);
+			var absB = Math.Abs(b);
+			return absA > absB ? a : b;
+		}
+
+		public static float largest(float a, float b) {
+			var absA = Math.Abs(a);
+			var absB = Math.Abs(b);
+			return absA > absB ? a : b;
+		}
+
+		public static double largest(double a, double b) {
+			var absA = Math.Abs(a);
+			var absB = Math.Abs(b);
+			return absA > absB ? a : b;
+		}
+
 		public static int clamp(int value, int min, int max) {
 			return Math.Min(Math.Max(value, min), max);
 		}
-		
+
 		public static float clamp(float value, float min, float max) {
 			return Math.Min(Math.Max(value, min), max);
 		}
-		
+
 		public static double clamp(double value, double min, double max) {
 			return Math.Min(Math.Max(value, min), max);
 		}
@@ -46,7 +64,7 @@ namespace Code.Scripts.Util {
 			}
 			return min + (value - min) % (max - min);
 		}
-		
+
 		public static bool getByteFlag(byte data, int flag) {
 			if (flag is < 0 or > 8) {
 				throw new ArgumentException("Invalid byte flag index: " + flag);
