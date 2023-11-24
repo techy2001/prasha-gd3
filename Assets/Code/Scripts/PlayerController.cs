dbb8b81fc6f83df14640e98da954b566127b9bc4
AmyMialee <10999535+AmyMialee@users.noreply.github.com>
2023-11-24 10:22:01 +0000

    Player Camera

--- Assets/Code/Scripts/PlayerController.cs
+++ Assets/Code/Scripts/PlayerController.cs
@@ -1,5 +1,4 @@
-using System;
-using Code.Scripts.Util;
+using Cinemachine;
 using UnityEngine;
 
 namespace Code.Scripts {
@@ -13,6 +12,8 @@ namespace Code.Scripts {
 		[SerializeField] [Range(0f, 1280f)] private float accelerationAir = 20f;
 		[SerializeField] [Range(0f, 10f)] private float weight = 0.12f;
 		[SerializeField] [Range(0f, 64f)] private float jumpPower = 20f;
+		[SerializeField] private CinemachineVirtualCamera virtualCamera;
+		[SerializeField] private GameObject cameraTarget;
 		private CharacterController characterController;
 		private Vector3 inputDirection;
 		private Vector3 velocity;
@@ -24,8 +25,9 @@ namespace Code.Scripts {
 		}
 
 		private void Update() {
-			this.inputDirection =
-				Vector3.ClampMagnitude(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")), 1);
+			var cameraRotation = this.virtualCamera.transform.rotation.eulerAngles;
+			var inputRotation = Quaternion.Euler(0, cameraRotation.y, 0);
+			this.inputDirection = inputRotation * Vector3.ClampMagnitude(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")), 1);
 			this.jumpHeld = Input.GetButton("Jump");
 		}
 
@@ -50,6 +52,9 @@ namespace Code.Scripts {
 			if (Physics.Raycast(new Ray(this.transform.localPosition, Vector3.down), out var groundPoint, 1.2f) && this.velocity.y < 0) {
 				this.characterController.Move(groundPoint.point - this.transform.localPosition + Vector3.up * 1.0f);
 			}
+			
+			this.virtualCamera.transform.localPosition = Vector3.Lerp(this.virtualCamera.transform.localPosition, this.cameraTarget.transform.localPosition, 0.1f);
+			this.virtualCamera.transform.localRotation = Quaternion.Lerp(this.virtualCamera.transform.localRotation, this.cameraTarget.transform.localRotation, 0.1f);
 
 			var flatVelocity = this.velocity;
 			flatVelocity.y = 0;
