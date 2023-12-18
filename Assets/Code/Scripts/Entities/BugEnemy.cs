80e53d63e6c6e99c31e520bb093de3786ace6b81
Emmanuella-i <113349379+Emmanuella-i@users.noreply.github.com>
2023-12-18 05:31:14 +0000

    Added bug tracking & movement & death

--- Assets/Code/Scripts/Entities/BugEnemy.cs
+++ Assets/Code/Scripts/Entities/BugEnemy.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using Code.Scripts.Managers;
 using Code.Scripts.Player;
+using Code.Scripts.Util;
 using UnityEngine;
 
 public class BugEnemy : MonoBehaviour {
     public float damage;
-    
+    public float speed;
+    private Code.Scripts.Managers.GameController gameController;
+    [SerializeField] public AudioClip deathSound;
+
+    private void Awake()
+    {
+        this.gameController = FindObjectOfType<GameController>();
+    }
+
     void Start() {
         
     }
-    
-    private void OnTriggerEnter(Collider other) {
-        if (other.CompareTag("Player")) {
-            other.GetComponent<PlayerHealth>().TakeDamage(this.damage);
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(this.transform.position, GameObject.FindWithTag("Player").transform.position, speed);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject == this.gameController.player.gameObject)
+        {
+            var player = this.gameController.player;
+            if (player.velocity.y < 0)
+            {
+                player.velocity.y = -player.velocity.y;
+                AudioHelper.PlayNullableClip(this.deathSound, player.transform.position);
+                this.discard();
+            }
+            else
+            {
+                player.playerHealth.TakeDamage(1);
+                this.gameController.player.velocity *= -1;
+                this.gameController.player.velocity.y = 10;
+            }
         }
     }
+
+    private void discard()
+    {
+        
+        Destroy(this.gameObject);
+    }
 }
+
